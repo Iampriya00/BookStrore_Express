f37d6adc07f138ead6b9a65cc2baf5c359adc07b
Priya Chakraborty <priya@yourflow.com.au>
2024-11-12 23:18:11 +0530

    Commit: Frontend setup

---
+++ Frontend/Bookstore/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <>
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          BookHeaven
+        </nav>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
---
+++ Frontend/Bookstore/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>fuck</h1>
+    </div>
+  );
+}
+
+export default Home;

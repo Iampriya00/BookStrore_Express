2251ca540e18a8505327c1b1df86ea52a58532cf
Priya Chakraborty <priya@yourflow.com.au>
2025-01-30 12:24:50 +0530

    Commit: copyright

--- Frontend/Bookstore/src/components/Footer.jsx
+++ Frontend/Bookstore/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div className="p-4 text-center text-white bg-zinc-800">
-      <p>Copyright 2024</p>
+      <p>Copyright © 2025</p>
     </div>
   );
 }
